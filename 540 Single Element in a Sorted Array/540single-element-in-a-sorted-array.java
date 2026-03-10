2b601dcea26d894b1750b36de799b06245b77c46
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-03-10 19:11:50 +0000

    Code challenge submission for question: 540 Single Element in a Sorted Array

---
+++ 540 Single Element in a Sorted Array/540single-element-in-a-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int mid;
+        int high = nums.length-1;
+        while(low<high)
+        {
+            mid =(low+high)/2;
+            if(mid%2 ==1)
+                mid--;
+            if(nums[mid]==nums[mid+1])
+                low = mid+2;
+            else
+                high = mid;
+        }
+        return nums[high];
+    }
+}
