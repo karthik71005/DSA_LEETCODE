b856edeac4e60adf27063b7e3cb67bbe09aadff9
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-06-27 20:03:18 +0000

    Code challenge submission for question: 42 Trapping Rain Water

---
+++ 42 Trapping Rain Water/42trapping-rain-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int []leftmax = new int [height.length];
+        int []rightmax = new int [height.length];
+        rightmax[height.length-1] = height[height.length-1];
+         leftmax[0] = height[0];
+        for(int i =1;i<height.length;i++)
+        {
+            leftmax[i] = Math.max(leftmax[i-1],height[i]);
+        }
+        for(int i =height.length-2;i>=0;i--)
+        {
+            rightmax[i] = Math.max(rightmax[i+1],height[i]);
+        }
+
+        int count =0;
+        for(int i =0;i<height.length;i++)
+        {
+            count+=(Math.min(leftmax[i],rightmax[i])-height[i]);
+        }
+        return count;
+    }
+}
