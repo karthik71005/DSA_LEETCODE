1b8bc3cf52dfccf1260bf04451a9912e1547a898
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-09 20:04:41 +0530

    Time: 5 ms (61.61%), Space: 44.4 MB (74.85%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,28 +1,51 @@
+// class Solution {
+//     public boolean isAnagram1(String s, String t) {
+//         if(s.length()!=t.length())
+//         {
+//             return false;
+//         }
+//         int [] freq1 = new int[26];
+//         int [] freq2 = new int[26];
+//         for(int  i =0;i<s.length();i++)
+//         {
+//             freq1[(int)s.charAt(i)-'a']++;
+//             freq2[(int)t.charAt(i)-'a']++;
+//         }
+//         for(int i =0;i<26;i++)
+//         {
+//             if(freq1[i]!=freq2[i])
+//             {
+//                 return false;
+//             }
+
+//         }
+//         // if(freq[(int)s.charAt(0)-'a']!=freq[(int)t.charAt(0)-'a'])
+//         //     return false;
+//         return true;
+        
+
+//     }
+// }
 class Solution {
     public boolean isAnagram(String s, String t) {
+        int [] freq = new int[26];
         if(s.length()!=t.length())
         {
             return false;
         }
-        int [] freq1 = new int[26];
-        int [] freq2 = new int[26];
-        for(int  i =0;i<s.length();i++)
+        for(int i =0;i<s.length();i++)
         {
-            freq1[(int)s.charAt(i)-'a']++;
-            freq2[(int)t.charAt(i)-'a']++;
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
         }
+        // if(freq[s.charAt(0)-'a']!=0)
+        //     return false;
         for(int i =0;i<26;i++)
         {
-            if(freq1[i]!=freq2[i])
-            {
+            if(freq[i]!=0)
                 return false;
-            }
-
         }
-        // if(freq[(int)s.charAt(0)-'a']!=freq[(int)t.charAt(0)-'a'])
-        //     return false;
         return true;
-        
 
     }
 }
