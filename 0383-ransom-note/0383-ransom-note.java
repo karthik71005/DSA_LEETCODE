f44cb62f698e9a85b04d5d128c06a0578b4c60d6
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-12 10:33:30 +0530

    Time: 14 ms (35.83%), Space: 47.2 MB (20.28%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> freq = new HashMap<>();
+        for(char ch:magazine.toCharArray())
+        {
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+        for(char ch:ransomNote.toCharArray())
+        {
+            if(!freq.containsKey(ch))
+            {
+                return false;
+            }
+            int num = freq.get(ch);
+            num--;
+            if(num==0)
+            {
+                freq.remove(ch);
+            }
+            else{
+            freq.put(ch,num);
+            }
+
+        }
+        return true;
+    }
+}
