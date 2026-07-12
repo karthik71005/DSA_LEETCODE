c6ac21c7497b40fafac6f091514dc5a9725a8280
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-12 19:49:58 +0530

    Time: 6 ms (32.38%), Space: 43.4 MB (53.38%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxNumberOfBalloons(String text) {
+    public int maxNumberOfBalloons1(String text) {
         HashMap <Character,Integer> freq =new HashMap<>();
         for(char ch:text.toCharArray())
         {
@@ -27,5 +27,21 @@ class Solution {
             }
         }
         return ans;
+    }
+    public int maxNumberOfBalloons(String text) {
+        HashMap <Character,Integer> freq =new HashMap<>();
+        for(char ch:text.toCharArray())
+        {
+            if(ch=='b'||ch=='a'||ch=='l'||ch=='o'||ch=='n')
+                freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+      
+        int b = freq.getOrDefault('b', 0);
+        int a = freq.getOrDefault('a', 0);
+        int l = freq.getOrDefault('l', 0) / 2;
+        int o = freq.getOrDefault('o', 0) / 2;
+        int n = freq.getOrDefault('n', 0);
+
+        return Math.min(Math.min(b, a), Math.min(Math.min(l, o), n));
     }
 }
