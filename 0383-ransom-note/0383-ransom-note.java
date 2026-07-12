691d74c09081fe9ba94675661259c7baffc85d60
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-12 10:36:32 +0530

    Time: 1 ms (99.98%), Space: 46.4 MB (68.88%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
+    public boolean canConstructlazy(String ransomNote, String magazine) {
         HashMap<Character,Integer> freq = new HashMap<>();
         if (ransomNote.length() > magazine.length()) {
             return false;
@@ -27,4 +27,29 @@ class Solution {
         }
         return true;
     }
+    public boolean canConstruct(String ransomNote, String magazine) {
+        // Quick optimization: If the note requires more letters than the magazine has, it's impossible
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        // A fixed-size array acts as a perfect direct-address map for 'a' through 'z'
+        int[] counts = new int[26];
+
+        // Fill counts from the magazine
+        for (char ch : magazine.toCharArray()) {
+            counts[ch - 'a']++;
+        }
+
+        // Consume counts for the ransomNote
+        for (char ch : ransomNote.toCharArray()) {
+            // Decrement first, then check if we've used more than available
+            if (--counts[ch - 'a'] < 0) {
+                return false; // Immediate failure exit
+            }
+        }
+
+        return true;
+    }
+    
 }
