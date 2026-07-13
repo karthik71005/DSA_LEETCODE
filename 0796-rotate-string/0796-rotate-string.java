697891c95a96fa52b0c480063812e194041366de
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-13 18:34:47 +0530

    Time: 1 ms (51.49%), Space: 42.8 MB (63.97%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())
+            return false;
+        
+        String doubled =s+s;
+        int n = goal.length();
+        for(int i =0;i<=n;i++)
+        {
+            boolean matchFound = true;
+            for(int j =0;j<n;j++)
+            {
+                if(doubled.charAt(i+j)!=goal.charAt(j)){
+                    matchFound = false;
+                    break;
+                }
+            }
+            if(matchFound)
+                return true;
+        }
+        return false;
+    }
+
+}
