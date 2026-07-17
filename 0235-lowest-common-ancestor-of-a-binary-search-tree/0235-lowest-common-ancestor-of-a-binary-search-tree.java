19b8628ca598144a04b45b77cac8527c83b9159d
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-18 02:22:19 +0530

    Time: 6 ms (97.08%), Space: 47.9 MB (16.84%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,32 +1,46 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode(int x) { val = x; }
+//  * }
+//  */
 
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode temp = root;
-        while(temp!=null)
-        {
-            if(p.val<temp.val && q.val<temp.val)
-            {
-                temp =temp.left;
-            }
-            else if(p.val>temp.val && q.val>temp.val)
-            {
-                temp =temp.right;
-            }
-            else {
-                return temp;
-            }
-        }
-      return null;
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         TreeNode temp = root;
+//         while(temp!=null)
+//         {
+//             if(p.val<temp.val && q.val<temp.val)
+//             {
+//                 temp =temp.left;
+//             }
+//             else if(p.val>temp.val && q.val>temp.val)
+//             {
+//                 temp =temp.right;
+//             }
+//             else {
+//                 return temp;
+//             }
+//         }
+//       return null;
   
         
+//     }
+// }
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+ 
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+ 
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    
+        return root;
     }
 }
