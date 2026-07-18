4587cdfd4f198d95645d520cb0167ac922b55587
Karthik Acharya <161597493+karthik71005@users.noreply.github.com>
2026-07-18 12:21:43 +0530

    Time: 1 ms (36.79%), Space: 46.7 MB (69.14%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+
+ //one method you can do is do inoreder rtrvaersla and then get k-1 th element from the lis here the time and sppace complexity is o(n)
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+        Stack <TreeNode> stck = new Stack<>();
+        TreeNode curr = root;
+        while(curr!=null || !stck.isEmpty())
+        {
+            
+             while(curr!=null)
+             {
+                stck.push(curr);
+                curr = curr.left;
+             }
+             TreeNode temp = stck.pop();
+             k--;
+             if(k==0)
+                return temp.val;
+            curr = temp.right;
+        }
+    return -1;    
+    }
+    
+
+}
